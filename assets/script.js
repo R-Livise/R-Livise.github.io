3a20b40f833d628ac8a6c664fe51529efd0c19b4
Rafael Livise <r.livise@hotmail.com>
2020-08-13 01:02:00 -0500

    Desplazamiento por con el navegador

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,36 @@
-console.log("prueba");
+function redirigir(ev) {
+    let section = ev.target.id
+
+    document.getElementById('nav-home').classList.remove("resalta")
+    document.getElementById('nav-work').classList.remove("resalta")
+    document.getElementById('nav-contact').classList.remove("resalta")
+
+    ev.target.classList.add("resalta")
+    //window.location.hash = '#' + section.split("-")[1];
+
+    let div = document.getElementById(section.split("-")[1])
+    console.log(div.offsetTop)
+    if (div != null) {
+        document.scrollingElement.scrollTop = div.offsetTop - 100;
+    }
+
+}
+
+function redirigirFocus(ev) {
+    if (ev.key == "Enter") {
+        redirigir(ev);
+
+    }
+
+}
+
+function prueba(ev) {
+    console.log(ev)
+}
+
+document.getElementById('nav-home').addEventListener('click', redirigir)
+document.getElementById('nav-work').addEventListener('click', redirigir)
+document.getElementById('nav-contact').addEventListener('click', redirigir)
+document.getElementById('nav-home').addEventListener('keyup', redirigirFocus)
+document.getElementById('nav-work').addEventListener('keyup', redirigirFocus)
+document.getElementById('nav-contact').addEventListener('keyup', redirigirFocus)
